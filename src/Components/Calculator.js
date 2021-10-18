b3d365fcd0abb9202817734d5facc617532101a9
SamanAtashi <milady00126@gmail.com>
2021-10-18 10:07:00 +0300

    Add position to root element

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -1,48 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Calculator.css';
 
-class Calculator extends React.PureComponent {
-  render() {
-    const list = [
-      '1',
-      '2',
-      '3',
-      '4',
-      '5',
-      '6',
-      '7',
-      '8',
-      '9',
-      'AC',
-      '+/-',
-      '%',
-      '÷',
-      '.',
-      'x',
-      '+',
-      '-',
-      '=',
-    ];
+class Calculator extends Component {
+list = [
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  'AC',
+  '+/-',
+  '%',
+  '÷',
+  '.',
+  'x',
+  '+',
+  '-',
+  '=',
+];
 
-    const listItem = list.map((item, index) => (
-      <li
-        key={item.toString()}
-        className={`calc-item o-${index + 1}`}
-      >
-        <button type="button">{item}</button>
+listItem = this.list.map((item, index) => (
+  <li
+    key={item.toString()}
+    className={`calc-item o-${index + 1}`}
+  >
+    <button type="button">{item}</button>
+  </li>
+));
+
+render() {
+  return (
+    <ul className="d-flex">
+      <li className="calc-result d-flex o-0">0</li>
+      {this.listItem}
+      <li className="calc-item calc-zero o-19">
+        <button type="button">0</button>
       </li>
-    ));
-
-    return (
-      <ul className="d-flex">
-        <li className="calc-result d-flex o-0">0</li>
-        {listItem}
-        <li className="calc-item calc-zero o-19">
-          <button type="button">0</button>
-        </li>
-      </ul>
-    );
-  }
+    </ul>
+  );
+}
 }
 
 export default Calculator;
