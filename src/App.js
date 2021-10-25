fd7af7cad523cdd07ec0d7129916d5fa1272b40d
SamanAtashi <milady00126@gmail.com>
2021-10-25 10:31:10 +0300

    Add switch to app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-import './App.css';
 import Calculator from './Components/Calculator';
+// import Home from './pages/Home';
+// import Quote from './pages/Quote';
+import './App.css';
+import Navbar from './Components/Navbar';
 
-const App = () => (<Calculator />);
+const App = () => (
+  <>
+    <Switch>
+      <Navbar />
+      <Route path="/calculator">
+        <div className="App">
+          <h2>Let&apos;s do some Calculation!</h2>
+          <Calculator />
+        </div>
+      </Route>
+      {/* <Route path="/quote">
+        <Quote />
+      </Route>
+      <Route exact path="/">
+        <Home />
+      </Route> */}
+    </Switch>
+  </>
+);
 
 export default App;
